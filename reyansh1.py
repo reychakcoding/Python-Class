19aaf0e7538f35a62da8f902a06910ffb8bb6284
reychakcoding <reyansh.chakka@gmail.com>
2024-05-26 17:33:11 -0500

    my first commit

---
+++ reyansh1.py
@@ -0,0 +1,14 @@
+name = input("What is your name?")
+
+if name == "Anvay":
+    print("Hi, Anvay!")
+    print("it works!")
+elif name == "Reyansh":
+    print("Hi, Reyansh!")
+    print("it works!")
+elif name == "Arav":
+    print("Hi, Arav!")
+    print("it works!")
+else:
+    print("Hello, " + name + "!")
+    
