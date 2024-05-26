fe6ed88f01e84d533e985635c5e77c449744f084
12Firestar <anvaymathur007@gmail.com>
2024-05-26 18:27:12 -0400

    third try

--- anvay1.py
+++ anvay1.py
@@ -1,7 +1,7 @@
 name = input("what is your name?")
 
-if name == "Anvay" or "anvay":
+if name == "Anvay" or name == "anvay":
     print("Hello Anvay!")
-elif name == "Arav" or "arav":
+elif name == "Arav" or name == "arav":
     print("Hello Arav!")
 else: ("Hello Reyansh!")
