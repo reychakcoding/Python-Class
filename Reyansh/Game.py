dc1c68dabd1a201f8914c0e007e9e6f33acf12c3
reychakcoding <reyansh.chakka@gmail.com>
2024-10-27 18:55:28 -0500

    jetpackadded.py

--- Reyansh/Game.py
+++ Reyansh/Game.py
@@ -13,6 +13,7 @@ font = py.font.Font(None,20)
 score = 0
 xspeed = 0
 yspeed = 0
+jetpack = 100
 # can access multiple properties of the rect
 # top, bottom, right,left,top-right, x,y,width,height
 
@@ -22,19 +23,24 @@ while True:
     ev = py.event.poll() # gets the latest event
     if ev.type == py.QUIT:
         break
-    keys = py.key.get_pressed() # a list of all current keys pressed
-    if keys[py.K_a]:
-      xspeed = 2
-      yspeed = 0
-    if keys[py.K_d]:
-      xspeed= -2
-      yspeed = 0
-    if keys[py.K_s]:
-      yspeed = 2
-      xspeed = 0
-    if keys[py.K_w]:
-      yspeed = -2 
-      xspeed = 0
+    keys = py.key.get_pressed()# a list of all current keys pressed
+    if (jetpack > 0):
+      if keys[py.K_a]:
+        xspeed = 2
+        yspeed = 0
+        jetpack=jetpack-0.25
+      if keys[py.K_d]:
+        xspeed= -2
+        yspeed = 0
+        jetpack=jetpack-0.25
+      if keys[py.K_s]:
+        yspeed = 2
+        xspeed = 0
+        jetpack=jetpack-0.25
+      if keys[py.K_w]:
+        yspeed = -2 
+        xspeed = 0
+        jetpack=jetpack-0.25
     rock.top-=yspeed
     rock.left+=xspeed
     if(player.colliderect(rock)==True):  #if blue square collided with black square
@@ -52,7 +58,7 @@ while True:
     # MOUSEBUTTONDOWN   pos, button, touch
     
     window.fill("white")
-    text = font.render("Your score is " + str(score) +" .",True,(0,0,0))
+    text = font.render("Your score is " + str(score) +". Jetpack fuel is " + str(jetpack),True,(0,0,0))
     mouseX,mouseY = py.mouse.get_pos()
 
     #player.centerx = mouseX
