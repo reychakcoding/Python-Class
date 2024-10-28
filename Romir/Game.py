c7889a5712476080c2caa39614778ca427c38514
romir <105827309+Romir01@users.noreply.github.com>
2024-10-27 20:01:21 -0400

    Update Game.py

--- Romir/Game.py
+++ Romir/Game.py
@@ -1,4 +1,7 @@
+
 import pygame as py
+import math
+
 #setup
 py.init()
 windowwidth = 500
@@ -6,10 +9,14 @@ windowheight = 500
 window = py.display.set_mode((windowwidth,windowheight))
 py.display.set_caption("My First Game")
 clock = py.time.Clock()
-player = py.Rect(20,20,30,30) #x,y,width,height
+player = py.Rect(20,20,30,30)
+bullet = py.Rect(50,50,10,10)#x,y,width,height
 zombie = py.Rect(400,400,20,20)
 # can access multiple properties of the rect
 # top, bottom, right,left,top-right, x,y,width,height
+
+def getangle(x1,y1,x2,y2):
+  return math.atan2(y2-y1,x2-x1)
 
 #draw loop
 while True:
@@ -29,7 +36,7 @@ while True:
 
       
     if ev.type == py.MOUSEBUTTONDOWN:
-      print("mouse is clicked")
+      print(getangle(player.centerx, player.centery, mouseX, mouseY))
     # event options
     # QUIT              none
     # KEYDOWN           key, mod, unicode, scancode
@@ -42,12 +49,27 @@ while True:
     mouseX,mouseY = py.mouse.get_pos()
 
 
+  
+
     #player.centerx = mouseX
     #player.centery = mouseY
 
 
+    if player.right >= 500:
+      player.right -= 10
+      
+    if player.left <= 0:
+      player.left += 10
+      
+    if player.bottom >= 500:
+      player.bottom -= 10
+      
+    if player.top <= 0:
+      player.top += 10
+
     #draw here
     py.draw.rect(window,(0,0,0),player)
+    py.draw.rect(window,(255,0,0),bullet)
     py.draw.rect(window,(0,255,0),zombie)# surface,color (RGB), x,y,width,height
     py.display.flip()
     clock.tick(60)
